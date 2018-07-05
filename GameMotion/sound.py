a2b57a4824279780360047c8544477def4dad48f
squigglemufkinz <sum11011@byui.edu>
2018-07-05 15:26:21 -0600

    Changed "Animation" file to "GameMotion"
    Github is finnicky about the name for some reason

---
+++ GameMotion/sound.py
@@ -0,0 +1,17 @@
+import pygame
+import time
+
+pygame.display.init()
+pygame.display.set_mode((10, 10))
+
+pygame.mixer.init()
+
+coin = pygame.mixer.Sound("Rainy.ogg")
+# pygame.mixer.load("TheLoomingBattle.OGG")
+# pygame.music.play()
+
+coin.play()
+
+time.sleep(30)
+
+pygame.quit()
