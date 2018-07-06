4c2e81ba409e333aed9ecd14a2d2c308e5c7b0b1
Michael Kellett <michaelk@gokicomputing.com>
2018-07-05 18:32:33 -0600

    action screen pseudo-code

--- MICHAEL'S TOYBOX/BattleScreen/ActionMenu.py
+++ MICHAEL'S TOYBOX/BattleScreen/ActionMenu.py
@@ -1,3 +1,42 @@
 """
-    This Program
+ACTION SCREEN
+
+    1) Draw ACTION SCREEN
+
+    2) Display ENEMY
+
+        1) Display NAME
+
+        2) Display HP
+
+        3) Display SPRITE
+
+    3) Display ALLY (Inherits from Enemy perhaps)
+
+        1) CHECK for Ally
+
+        1) Display NAME
+
+        2) Display HP
+
+        3) Display SPRITE
+
+    4) Display ACTIVITY BOX
+
+        a) Display ENEMY TEXT
+
+        b) Display ACTION BOX
+
+            i)      Attack
+
+                        i) Display ATTACK RESULTS
+
+            ii)     Check
+
+                        i) Display ENEMY TEXT
+
+            iii)    Leave
+
+                        a) EXIT Action Screen
+
 """
---
+++ MICHAEL'S TOYBOX/BattleScreen/TextBox.py
@@ -0,0 +1,37 @@
+import pygame
+import sys
+
+
+white = (255,255,255)
+black = (0,0,0)
+
+
+class Pane(object):
+    def __init__(self):
+        pygame.init()
+        self.font = pygame.font.SysFont('Arial', 25)
+        pygame.display.set_caption('Box Test')
+        self.screen = pygame.display.set_mode((600,400), 0, 32)
+        self.screen.fill(white)
+        pygame.display.update()
+
+
+    def addRect(self):
+        # dimensions are X,Y, X2,Y2
+        self.rect = pygame.draw.rect(self.screen, black, (0, 300, 600, 400))
+        pygame.display.update()
+
+    def addText(self):
+        # can display 54 characters / line
+        # render ((content, [not sure what True does] (color of font)) (location to start drawing at) 
+        self.screen.blit(self.font.render('Testing one two three', True, (255,0,0)), (2, 300))
+        pygame.display.update()
+
+if __name__ == '__main__':
+    Pan3 = Pane()
+    Pan3.addRect()
+    Pan3.addText()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit(); sys.exit();
